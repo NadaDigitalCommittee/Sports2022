d0ad44b477f71ce8707294afacbe51883c6db64a
testuser1111111-nd <testuser1111111-nd@users.noreply.github.com>
2022-09-05 20:15:30 +0000

    deploy: fdaf86495cdf6ba700528b4e84f6dca6b4306749

---
+++ _nuxt/static/1662408929/404/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSports2022\u002F_nuxt\u002Fstatic\u002F1662408929",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{_app:{basePath:"\u002FSports2022\u002F",assetsPath:"\u002FSports2022\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
