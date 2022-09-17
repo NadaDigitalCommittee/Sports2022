e9d57f011b337354db42f00d0aba5b0279257668
デカブツ <tbsa334zn6@outlook.jp>
2022-09-17 11:12:24 +0900

    fixed design

--- pages/article/design.vue
+++ pages/article/design.vue
@@ -1,42 +1,37 @@
 <template>
-    <div>
-        <Header/>
-        <div class="body">
-        <p class="title"><span class="iti">デ</span>ザイン課</p>
+    <div class="bg">
+      <Header/>
+      <div class="speech">
         <div class="concept">
-         <div class="profile">
-          <img src="~/assets/img/josuke.png" alt="デザイン課長の顔写真" class="face">
-           <div class="names">
-           <p class="role">デザイン課長</p>
-           <div class="pl"/>
-           <p class="name">髙島晟輔</p>
-           <div class="pl2"/>
-           </div>
-         </div> 
-        </div>
-        <p class="aisatu">デザイン課長の髙島です。デザイン課はロゴをはじめとし、うちわやパンフレット、今皆さんにみていただいている体育祭HPのデザインまで多種多様なデザインを担当しました。特に力を入れたのはロゴデザインです。コロナから脱却し、新たな体育祭を開拓するこの第93回灘校体育祭の力強さを、飛沫や線を効果的に用いて表現しました。灘校体育祭を彩る象徴として皆さんに愛されるロゴになれば、これほど嬉しいことはありません。</p>
-        <p class="aisatu">さて、何度も述べている通り、ついに今年の体育祭は無制限開催にたどり着くことができました。これまで灘校が紡いできた92回もの体育祭の伝統。そしてコロナをきっかけに芽生えた新たな体育祭の形。僕らはこれらを融合させ、体育祭に関わるすべての人が最高に盛り上がる、「めちゃくちゃおもろい」体育祭を作り出します。第93回灘校体育祭「拓」、発車の合図は間も無くです。</p>
-        </div>
+          <img src="~/assets/img/forarticle/design.png" alt="concept" class="midasi">
+          <div class="profile">
+            <img src="~/assets/img/josuke.png" alt="デザイン課長の顔写真" class="face">
+            <div class="names">
+              <p class="role">デザイン課長</p>
+              <div class="pl"/>
+              <p class="name">髙島晟輔</p>
+              <div class="pl2"/>
+            </div>
+          </div>
+          <p class="aisatu">デザイン課長の髙島です。デザイン課はロゴをはじめとし、うちわやパンフレット、今皆さんにみていただいている体育祭HPのデザインまで多種多様なデザインを担当しました。特に力を入れたのはロゴデザインです。コロナから脱却し、新たな体育祭を開拓するこの第93回灘校体育祭の力強さを、飛沫や線を効果的に用いて表現しました。灘校体育祭を彩る象徴として皆さんに愛されるロゴになれば、これほど嬉しいことはありません。<br/><br/>さて、何度も述べている通り、ついに今年の体育祭は無制限開催にたどり着くことができました。これまで灘校が紡いできた92回もの体育祭の伝統。そしてコロナをきっかけに芽生えた新たな体育祭の形。僕らはこれらを融合させ、体育祭に関わるすべての人が最高に盛り上がる、「めちゃくちゃおもろい」体育祭を作り出します。第93回灘校体育祭「拓」、発車の合図は間も無くです。</p>
+      </div>
+      </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.title{
- font-size: 50px;
- font-family: sans-serif;
- margin: auto;
- color: #000000;
- .iti{
-  font-size: 40px;
-  font-family: sans-serif;
-  margin: auto;
-  color: #FF4E00;
+.speech{
+  width: 100vw;
+  margin-top: 20px;
+  .midasi{
+    position: relative;
+    left: 10px;
+    height: 40px;
   }
- } 
-.concept{
-    margin: auto;
+  .concept{
+    width: min(100vw,max(75vw,600px));
+    margin: 0 auto;
     text-align: left;
-    width: min(100vw,max(70vw,600px));
     margin-bottom: 50px;
     img{
       border-color: $subcolor;
@@ -68,7 +63,7 @@
     justify-content: center;
     font-family: source-han-serif-japanese, serif;
     font-weight: 900;
-    width: min(100vw,max(50% , 600px));
+    width: min(100vw,max(60% , 600px));
     img{
       border-radius:50% ;
       border-width: 3px;
@@ -80,6 +75,7 @@
       margin-top: 15px;
     }
   }
+}
 .pl{
   position: relative;
   top: -12px;
@@ -99,14 +95,13 @@
 }
 .aisatu{
   font-size: 16px;
-  font-family: sans-serif;
+  font-family: "游ゴシック体", YuGothic, "游ゴシック", "Yu Gothic", sans-serif;
   margin: auto;
   padding-right: 15px;
   padding-left:15px;
   text-align: center;
-  width: calc( min(100vw,max(50% , 600px)) - 30px);
+  width: calc( min(100vw,max(70% , 600px)) - 30px);
 }
-
 </style>
 
 <script lang="ts">
