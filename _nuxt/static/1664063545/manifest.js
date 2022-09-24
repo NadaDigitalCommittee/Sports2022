33bea80431cc943dad261f075855773802f598f1
testuser1111111-nd <testuser1111111-nd@users.noreply.github.com>
2022-09-24 23:52:27 +0000

    deploy: 1874aa887be52398c1c90f13ff8af6b023418202

---
+++ _nuxt/static/1664063545/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Farticle\u002Fcheer","\u002F404","\u002Fpoints","\u002Fnews","\u002Farticles","\u002Fprograms","\u002Farticle\u002Fdesign","\u002Farticle\u002Ffield","\u002Farticle\u002Fdelivery","\u002Farticle\u002Fjudge","\u002Farticle\u002Flead","\u002Farticle\u002Fsumo"]})
